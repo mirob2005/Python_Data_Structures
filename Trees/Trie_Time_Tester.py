d96dc0487a4d0f3e653714ef00f7a1e83ef7de6f
Michael Robertson <mirob2005@gmail.com>
2013-03-05 15:44:53 -0800

    Added sample text to parse and use for tesing. Trie tester parses test and compares timing for 2 version of prefix trie.

---
+++ Trees/Trie_Time_Tester.py
@@ -0,0 +1,36 @@
+import time
+from Trie_Prefix_Count import Trie as Trie_space
+from Trie_Prefix_Count_Speed import Trie as Trie_speed
+
+t = Trie_space()
+
+start = time.clock()
+print('Start Trie Space: %s'%start)
+
+fileIn = open('../Lorem_ipsum.txt','r').read()
+words = fileIn.split(' ')
+
+for word in words:
+    t.add(word)
+    
+wordTime = time.clock()
+print('Words added in: %f'%(wordTime-start))
+
+wordFindTime = time.clock()
+print('Found pellentesque value = %s in %f'%(t.getValue('pellentesque'),(wordFindTime - wordTime)))
+
+t = Trie_speed()
+start = time.clock()
+print('Start Trie Speed: %f'%start)
+
+fileIn = open('../Lorem_ipsum.txt','r').read()
+words = fileIn.split(' ')
+
+for word in words:
+    t.add(word)
+    
+wordTime = time.clock()
+print('Words added in: %f'%(wordTime-start))
+
+wordFindTime = time.clock()
+print('Found pellentesque value = %s in %f'%(t.getValue('pellentesque'),(wordFindTime - wordTime)))
