caeabbc00ba46ef8540814378f3a70a2c648f34d
Michael Robertson <mirob2005@gmail.com>
2013-04-02 19:49:29 -0700

    AdjMatrix: Added DFS

--- Graphs/adjMatrix.py
+++ Graphs/adjMatrix.py
@@ -7,7 +7,7 @@
 # Can declare the graph as directed/undirected in the constructor.
 # Undirected prohibits self-loops whereas directed does not.
 # Undirected affects the indices for both A->B and B->A whereas directed
-#   only affects the direct declared in the addEdge command
+#   only affects the direction declared in the addEdge command
 
 from ADTs.Queue_head import Queue
 
@@ -40,9 +40,13 @@ class AdjMatrix:
         if vertex in self.vertexList:
             print('Vertex %s already exists'%vertex)
             return False
+        #Vertex List keeps track of the vertex name and the corresponding index
+        #   into the adjacency matrix
         self.vertexList[vertex] = self.numVertices
         self.numVertices += 1
+        #Add row to matrix
         self.matrix.append([])
+        #Fill out matrix to be square
         for row in self.matrix:
             while len(row) != self.numVertices:
                 row.append(False)
@@ -96,18 +100,51 @@ class AdjMatrix:
         if not vertex in self.vertexList:
             print('Vertex %s does not exist'%vertex)
             return False
+        #Remove vertex from list
         index = self.vertexList.pop(vertex)
+        #Remove corresponding column from matrix
         for row in self.matrix:
             row.pop(index)
+        #Remove corresponding row from matrix
         self.matrix.pop(index)
         self.numVertices -= 1
+        #Reduce the index of all vertices after the removed vertex to keep
+        #   matrix and indices organized 
         for key in self.vertexList.keys():
             if self.vertexList[key] > index:
                 self.vertexList[key] -= 1
         return True
     
-    def traverseDFS(self,source):
-        pass
+    def traverseDFS(self):
+        #Used to keep track if vertex is undiscovered(None), discovered(False),
+        #   or fully explored (True)
+        visit = {}
+        string = ''
+        for vertex in self.vertexList.keys():
+            visit[vertex] = None
+        for vertex in sorted(visit):
+            #If any undiscovered vertices remain, they become the new source
+            if visit[vertex] == None:
+                string += self.DFS(vertex,visit)
+        return string
+        
+    def DFS(self,source,visit):
+        visit[source] = False
+        string = '(%s '%source
+        #Used to index into the adjacency matrix
+        index = self.vertexList[source]
+        destIndex = 0
+        for edge in self.matrix[index]:
+            if edge:
+                for key,value in self.vertexList.items():
+                    if value == destIndex:
+                        #If vertex is undiscovered
+                        if visit[key] == None:
+                            string += self.DFS(key,visit)
+            destIndex+=1
+        visit[source] = True
+        string += ' %s)'%source
+        return string
     
     def traverseBFS(self,source):
         bfsQ = Queue()
@@ -119,12 +156,14 @@ class AdjMatrix:
         cur = bfsQ.deQueue()
         distanceTo[cur] = 0
         while cur!=None:
+            #Used to index into the adjacency matrix
             index = self.vertexList[cur]
             destIndex = 0
             for edge in self.matrix[index]:
                 if edge:
                     for key,value in self.vertexList.items():
                         if value == destIndex:
+                            #If vertex is undiscovered
                             if not key in distanceTo:
                                 bfsQ.enQueue(key)
                                 distanceTo[key] = distanceTo[cur]+1
@@ -139,10 +178,21 @@ if __name__ == '__main__':
     for vertex in ['r','s','t','u','v','w','x','y']:
         am.addVertex(vertex)
     
-    print(am)
-    
     for source,dest in [('r','v'),('r','s'),('s','w'),('w','t'),('w','x'),('t','x'),('t','u'),('x','u'),('x','y'),('u','y')]:
         am.addEdge(source,dest)
+        
     print(am)
-    
     print(am.traverseBFS('r'))
+    print(am.traverseDFS())
+    print('\n_________________________________\n')
+    
+    dfs = AdjMatrix(True)
+    for vertex in ['y','z','s','t','x','w','v','u']:
+        dfs.addVertex(vertex)
+        
+    for source,dest in [('s','z'),('z','y'),('y','x'),('x','z'),('w','x'),('z','w'),('s','w'),('v','w'),('v','s'),('t','v'),('u','v'),('u','t'),('t','u')]:
+        dfs.addEdge(source,dest)
+        
+    print(dfs)
+    print(dfs.traverseBFS('s'))
+    print(dfs.traverseDFS())
